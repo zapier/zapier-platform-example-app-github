0bbd1c8d6ac5feaa186683195ea8f8c79acd09d4
Petr Cvengros <petr@cvengros.cz>
2018-03-12 16:07:46 +0100

    Bugfix - when creating an issue, body was always empty
    Added body to the issue create and provided a test.

--- test/issue.js
+++ test/issue.js
@@ -46,7 +46,8 @@ describe('issue trigger', () => {
     };
     appTester(App.creates.issue.operation.perform, bundle)
       .then((response) => {
-
+        should.exist(response.title);
+        should.exist(response.body);
         done();
       })
       .catch(done);
