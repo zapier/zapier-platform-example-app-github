c0ccf9d07cf79d59abb3cc2f51889434982d44b8
Emmanuel <emmanuelnlopez+15@gmail.com>
2019-03-01 10:50:16 -0800

    Did the guide

--- test/issue.js
+++ test/issue.js
@@ -6,7 +6,7 @@ const zapier = require('zapier-platform-core');
 const App = require('../index');
 const appTester = zapier.createAppTester(App);
 
-const REPO = 'username/reponame' // CHANGE THIS
+const REPO = 'emnolope/zapier-platform-example-app-github'
 
 //These are automated tests for the Issue create and Issue Trigger.
 //They will run every time the `zapier test` command is executed.
